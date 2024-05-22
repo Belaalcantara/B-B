1a4e2eaf877c9d7f3471a43ac1562de546de4daa
Isabela alcantara ferreira <isabela.ferreira16@aluno.senai.br>
2024-05-22 17:21:23 -0300

    feat: finalizacao components

--- src/components/DetalhesComidas/index.jsx
+++ src/components/DetalhesComidas/index.jsx
@@ -6,17 +6,22 @@ function Cardapio({ imagemHeader, imagemCard, subtitulo, subtitulo2, sobre, prec
 
         <View style={style.container}>
             <View style={style.cardapio}>
-                <Image style={style.imagemHeader}>{imagemCard}</Image>
+                <Image style={style.imagemHeader} source={imagemHeader}/>
                 <Text style={style.restaurante}>{restaurante}</Text>
                 <Text style={style.subtitulo}>{subtitulo}</Text>
-                </View >
-                <View style={style. cardCardapio}> 
-                <Image style={style.imagemCard}>{imagemCard}</Image>
-                <Text style={style.subtitulo2}>{subtitulo2}</Text>
-                <Text style={style.sobre}>{sobre}</Text>
+            </View >
+            <View style={style.cardCardapio}>
+                <View style={style.infos}>
+                <Image style={style.imagemCard} source={imagemCard}/>
+                <View style={style.infos2}>
+                <Text style={style.subtitulo2}>{subtitulo2}</Text> 
                 <Text style={style.preco}>{preco}</Text>
-</View>
-           
+                </View>
+                </View>
+                <Text style={style.sobre}>{sobre}</Text>
+                
+            </View>
+
         </View>
     )
 }
