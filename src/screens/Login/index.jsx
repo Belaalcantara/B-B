a96fcdd4160ad3e791f3a9d29d629eca9d952dfa
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-05-22 17:04:43 -0300

    feat: login

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -18,23 +18,26 @@ export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const fetchUser = async() => {
+    //user
+    const [user, setUser] = useState();
+
+    const fetchUser = async () => {
         try {
             const response = await axios.get(`http://localhost:4000/users/${email}`);
             const user = response.data;
-            console.log(user);
-            if(user) {
-               if(password == user.password) {
-                navigation.navigate('Home');
-               } else {
-                setPopup(true);
-                setContent('Erro! Senha incorreta');
-               }
+            if (user) {
+                if (password == user.password) {
+                    navigation.navigate('Home');
+                    setUser(user);
+                } else {
+                    setPopup(true);
+                    setContent('Erro! Senha incorreta');
+                }
             } else {
                 setPopup(true);
                 setContent('Erro! E-mail não cadastrado');
             }
-        } catch(e) {
+        } catch (e) {
             console.log('Erro interno de servidor', e);
             setPopup(true);
             setContent('Erro de servidor');
@@ -46,35 +49,45 @@ export default function Login() {
     }
 
 
-    return(
+    return (
         <View style={styles.container}>
-            <ImageBackground source={require('../../../assets/White and Red Minimalist Promotion Burger Food Instagram Story (1) (1).png')} resizeMode="cover" style={styles.background}/>
-            <View style={styles.form}>
-                <Text style={styles.title}>Faça seu login!</Text>
-                <View style={styles.content}>
-                    <View style={styles.inputfield}>
-                        <Text style={styles.label}>Email:</Text>
-                        <TextInput
-                            value={email}
-                            onChangeText={setEmail}
-                            keyboardType="email-address"
-                        />
+            {
+                user ? (
+                    <View style={styles.logged}>
+                        
                     </View>
-                    <View style={styles.inputfield}>
-                        <Text style={styles.label}>Senha:</Text>
-                        <TextInput
-                            value={password}
-                            onChangeText={setPassword}
-                        />
+                ) : (
+                    <View>
+                        <ImageBackground source={require('../../../assets/White and Red Minimalist Promotion Burger Food Instagram Story (1) (1).png')} resizeMode="cover" style={styles.background} />
+                        <View style={styles.form}>
+                            <Text style={styles.title}>Faça seu login!</Text>
+                            <View style={styles.content}>
+                                <View style={styles.inputfield}>
+                                    <Text style={styles.label}>Email:</Text>
+                                    <TextInput
+                                        value={email}
+                                        onChangeText={setEmail}
+                                        keyboardType="email-address"
+                                    />
+                                </View>
+                                <View style={styles.inputfield}>
+                                    <Text style={styles.label}>Senha:</Text>
+                                    <TextInput
+                                        value={password}
+                                        onChangeText={setPassword}
+                                    />
+                                </View>
+                                <TouchableOpacity onPress={fetchUser} style={styles.entryBtn}>
+                                    <Text style={{ color: 'white', textTransform: 'uppercase' }}>Entrar</Text>
+                                </TouchableOpacity>
+                                {
+                                    popup && <Popup type={content} />
+                                }
+                            </View>
+                        </View>
                     </View>
-                    <TouchableOpacity onPress={fetchUser} style={styles.entryBtn}>
-                        <Text style={{color:'white', textTransform: 'uppercase'}}>Entrar</Text>
-                    </TouchableOpacity>
-                    {
-                        popup && <Popup type={content}/>
-                    }
-                </View>
-            </View>
+                )
+            }
         </View>
     );
 }
