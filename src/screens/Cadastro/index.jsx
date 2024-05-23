0750c2363911e6e6e5c01f38f81f1548025f2761
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-05-23 09:56:18 -0300

    working: register page

---
+++ src/screens/Cadastro/index.jsx
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Register() {
+    const navigation = useNavigation();
+
+    return(
+        <View style={styles.container}>
+
+        </View>
+    );
+}
