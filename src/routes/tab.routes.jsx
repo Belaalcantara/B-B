b5eb1937a8d4074503599e73fab25256ac8187bc
Isabela alcantara ferreira <isabela.ferreira16@aluno.senai.br>
2024-05-27 08:32:03 -0300

    commit fix

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import Category from "../screens/Category";
-import Comentarios from '../screens/Comentarios'
-import RendComent from '../screens/RendComent'
+import Comentarios from '../screens/Comentarios';
+import RendComent from '../screens/RendComent';
 import { user } from "../data/Profile";
 import SobreNos from "../screens/SobreNos";
 import BoasVindas1 from "../screens/BoasVindas1";
@@ -52,7 +53,6 @@ const TabRoutes = () => {
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
       <Tab.Screen
         name="Category"
         component={Category}
@@ -69,26 +69,6 @@ const TabRoutes = () => {
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
-
-<Tab.Screen
-        name="Comentarios"
-        component={Comentarios}
-        initialParams={{comentario: null, edit: false}}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Feather
-              name="comentarios"
-              size={24}
-              />
-            ),
-            tabBarLabel: "Categorias",
-            tabBarActiveTintColor: "#131313",
-            tabBarInactiveTintColor: "#D6D6D6",
-        }}
-      />
-
-
       <Tab.Screen
         name="SobreNos"
         component={SobreNos}
@@ -97,123 +77,111 @@ const TabRoutes = () => {
             <Feather
               name="users"
               size={24}
-              />
-            ),
-            tabBarLabel: "Categorias",
-            tabBarActiveTintColor: "#131313",
-            tabBarInactiveTintColor: "#D6D6D6",
+              color={focused ? "#131313" : "#D6D6D6"}
+            />
+          ),
+          tabBarLabel: "Sobre Nós",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
-<Tab.Screen
+      <Tab.Screen
         name="BoasVindas1"
         component={BoasVindas1}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
-              name="list"
+              name="smile"
               size={24}
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-          tabBarLabel: "Categorias",
+          tabBarLabel: "Boas-Vindas 1",
           tabBarActiveTintColor: "#131313",
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
-<Tab.Screen
+      <Tab.Screen
         name="BoasVindas2"
         component={BoasVindas2}
         initialParams={{ isAlreadyRegistered: true }}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
-              name="list"
-
+              name="smile"
               size={24}
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-
-          tabBarLabel: "Inicial",
-
-          tabBarLabel: "Categorias",
-
+          tabBarLabel: "Boas-Vindas 2",
           tabBarActiveTintColor: "#131313",
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
-<Tab.Screen
-
-        name="RendComent"
-        component={RendComent}
-        
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Feather
-              name="comentarios"
-
+      <Tab.Screen
         name="BoasVindas3"
         component={BoasVindas3}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
-              name="list"
+              name="smile"
               size={24}
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-          tabBarLabel: "Categorias",
+          tabBarLabel: "Boas-Vindas 3",
           tabBarActiveTintColor: "#131313",
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
       <Tab.Screen
         name="Login"
         component={Login}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
-              name="user"
+              name="log-in"
               size={24}
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-          tabBarLabel: "Perfil",
+          tabBarLabel: "Login",
           tabBarActiveTintColor: "#131313",
           tabBarInactiveTintColor: "#D6D6D6",
         }}
       />
-
       <Tab.Screen
         name="Register"
         component={Register}
         options={{
           tabBarIcon: ({ focused }) => (
             <Feather
-              name="user"
-
+              name="user-plus"
               size={24}
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-
-          tabBarLabel: "Inicial",
-          tabBarActiveTintColor: "#131313",
-          tabBarInactiveTintColor: "#D6D6D6",
-        }}
-      />
-
           tabBarLabel: "Registrar",
           tabBarActiveTintColor: "#131313",
           tabBarInactiveTintColor: "#D6D6D6",
-          headerShown: false
         }}
       />
-
+      <Tab.Screen
+        name="RendComent"
+        component={RendComent}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="message-circle"
+              size={24}
+              color={focused ? "#131313" : "#D6D6D6"}
+            />
+          ),
+          tabBarLabel: "Comentários",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#D6D6D6",
+        }}
+      />
     </Tab.Navigator>
   );
 };
