e2c6525c31080604fdb1d882d6d81977483bb80a
Isabela alcantara ferreira <isabela.ferreira16@aluno.senai.br>
2024-05-27 07:52:25 -0300

    feat:commit teste

--- src/screens/Category/index.jsx
+++ src/screens/Category/index.jsx
@@ -6,25 +6,27 @@ import Cardapio from "../../components/DetalhesComidas";
 export default function CardapioScreen() {
   return (
     <ScrollView>
+
       <View style={style.cardapio}>
-  
+        <Image
+          style={[style.imageHeader, { width: 400, height: 188 }]}
+          source={require('../../../assets/spoleto-.webp')}
+        />
+      </View>
+      <View style={style.container}>
+        <View style={style.cardCardapio}>
 
-      <Cardapio
-  restaurante="Spoleto"
-  subtitulo="Preço mínimo de 30 reais"
-  subtitulo2="Café gelado"
-  sobre="De energia e matar o calor, do jeito que você mais precisa de todas as formas que quiser, sendo: fraco, médio, forte e extra forte."
-  preco="R$ 9,00"
->
-  <Image 
-    style={style.imageHeader}
-    source={require('../../../assets/cheese.jpg')}
-  />
-</Cardapio>
-
+          <Cardapio
+             imagemCard={require('../../../assets/spaghetti1.webp')}
+            restaurante="Spoleto"
+            subtitulo="Preço mínimo de 30 reais"
+            subtitulo2="Spaghetti" 
+            preco="R$ 9,00"
+            sobre=" fraco, médio, forte e extra forte."
+            
+          />
+        </View>
       </View>
     </ScrollView>
   );
 }
-
-
