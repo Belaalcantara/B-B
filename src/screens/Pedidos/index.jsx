90732642a6b791bb75c4f496192bad14485868c9
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2024-05-27 14:42:06 -0300

    wip: ordersPage

--- src/screens/Pedidos/index.jsx
+++ src/screens/Pedidos/index.jsx
@@ -1,17 +1,28 @@
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
+import { useState } from "react";
 
 export default function Orders() {
+    const [flag, setFlag] = useState(false);
+
     return (
         <View style={styles.container}>
             <Text>PEDIDOS</Text>
             <View style={styles.orders}>
-                <View style={styles.unique}>
-                    <Image/>
-                    <View style={styles.content}>
+                {
+                    flag ? (
+                        <View style={styles.unique}>
+                            <Image />
+                            <View style={styles.content}>
 
-                    </View>
-                </View>
+                            </View>
+                        </View>
+                    ) : (
+                        <View style={styles.loggon}>
+
+                        </View>
+                    )
+                }
             </View>
         </View>
     );
--- src/screens/Pedidos/styles.js
+++ src/screens/Pedidos/styles.js
@@ -3,6 +3,8 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
+        alignContent: 'center',
+        backgroundColor: '#681111'
     }
 });
 
