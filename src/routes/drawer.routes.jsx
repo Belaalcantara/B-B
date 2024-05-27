1fc7bb536c5be7247768d3cd7c21e6ad5616de3b
santni <nicolly.silva5@aluno.senai.br>
2024-05-27 09:21:01 -0300

    feat: alteraçoes 1

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -1,20 +1,41 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import React from 'react';
+import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 import { Feather } from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import SobreNos from "../screens/SobreNos";
 import Category from "../screens/Category";
-import BoasVindas1 from "../screens/BoasVindas1";
-import BoasVindas2 from "../screens/BoasVindas2";
-import BoasVindas3 from "../screens/BoasVindas3";
 import Login from "../screens/Login";
 import Register from "../screens/Cadastro";
 
 const Drawer = createDrawerNavigator();
 
+const CustomDrawerContent = (props) => {
+  return (
+    <ImageBackground source={require("../../assets/fundodrawer.png")} style={styles.background}>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+          <Image source={require("../../assets/igicon.png")} style={styles.profileImage} />
+          <Text style={styles.profileText}>Nome</Text>
+        </View>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </ImageBackground>
+  );
+};
+
 const DrawerRoutes = () => {
   return (
-    <Drawer.Navigator screenOptions={{ headerShown: true }}>
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerShown: true,
+        drawerStyle: {
+          backgroundColor: '#fff',
+        },
+      }}
+    >
       <Drawer.Screen
         name="Home"
         component={Home}
@@ -44,7 +65,7 @@ const DrawerRoutes = () => {
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-          drawerLabel: "Sobre Nós",
+          drawerLabel: "Equipe B&B",
           drawerActiveTintColor: "#131313",
           drawerInactiveTintColor: "#D6D6D6",
         }}
@@ -62,58 +83,6 @@ const DrawerRoutes = () => {
             />
           ),
           drawerLabel: "Categorias",
-          drawerActiveTintColor: "#131313",
-          drawerInactiveTintColor: "#D6D6D6",
-        }}
-      />
-      <Drawer.Screen
-        name="BoasVindas1"
-        component={BoasVindas1}
-        options={{
-          headerTitle: "",
-          drawerIcon: ({ focused }) => (
-            <Feather
-              name="list"
-              size={24}
-              color={focused ? "#131313" : "#D6D6D6"}
-            />
-          ),
-          drawerLabel: "Boas Vindas 1",
-          drawerActiveTintColor: "#131313",
-          drawerInactiveTintColor: "#D6D6D6",
-        }}
-      />
-      <Drawer.Screen
-        name="BoasVindas2"
-        component={BoasVindas2}
-        initialParams={{ isAlreadyRegistered: true }}
-        options={{
-          headerTitle: "",
-          drawerIcon: ({ focused }) => (
-            <Feather
-              name="list"
-              size={24}
-              color={focused ? "#131313" : "#D6D6D6"}
-            />
-          ),
-          drawerLabel: "Boas Vindas 2",
-          drawerActiveTintColor: "#131313",
-          drawerInactiveTintColor: "#D6D6D6",
-        }}
-      />
-      <Drawer.Screen
-        name="BoasVindas3"
-        component={BoasVindas3}
-        options={{
-          headerTitle: "",
-          drawerIcon: ({ focused }) => (
-            <Feather
-              name="list"
-              size={24}
-              color={focused ? "#131313" : "#D6D6D6"}
-            />
-          ),
-          drawerLabel: "Boas Vindas 3",
           drawerActiveTintColor: "#131313",
           drawerInactiveTintColor: "#D6D6D6",
         }}
@@ -147,7 +116,7 @@ const DrawerRoutes = () => {
               color={focused ? "#131313" : "#D6D6D6"}
             />
           ),
-          drawerLabel: "Registrar",
+          drawerLabel: "Cadastre-se",
           drawerActiveTintColor: "#131313",
           drawerInactiveTintColor: "#D6D6D6",
         }}
@@ -155,5 +124,32 @@ const DrawerRoutes = () => {
     </Drawer.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  drawerHeader: {
+    height: 150,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  profileText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default DrawerRoutes;
