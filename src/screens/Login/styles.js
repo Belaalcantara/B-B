42b8ce08a27108fee8e33db46dd1adeb0d22b5e7
santni <nicolly.silva5@aluno.senai.br>
2024-05-29 14:50:43 -0300

    feat: estilização da pá login

--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -14,29 +14,74 @@ const styles = StyleSheet.create({
     },
 
     form: {
-        backgroundColor:'white', 
-        display:'flex',
-        alignItems:'center',
-        padding:15,
-        borderRadius:8
+        backgroundColor: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        margin: '50px',
+        borderRadius: 8,
+        marginTop: '200px',
+        paddingTop: '40px',
+        paddingBottom: '40px',
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
     },
 
     label: {
-
+        color: 'gray',
+    },
+    title: {
+        marginLeft: '-125px',
+        fontSize: '30px',
+        fontWeight: 'bold',
+        marginBottom: '19px',
     },
 
     entryBtn: {
         backgroundColor: 'black',
         padding: 5,
-        borderRadius: 8,
+        borderRadius: 20,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         margin: 5
     },
 
+    btn: {
+        color: 'white',
+        textTransform: 'uppercase',
+        margin: '5px',
+    },
+
     nav: {
-        margin:5
+        margin: 5,
+    },
+    bluebtn: {
+        color: '#4D81E7',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        marginTop: '5px',
+    },
+    inpt: {
+        width: 200,
+        height: 40,
+        marginBottom: '20px',
+        borderRadius: 15,
+        backgroundColor: '#fff',
+        shadowColor: 'rgba(99, 99, 99, 1)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 4,
+
     }
 });
 
