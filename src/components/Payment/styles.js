b09534ce5505eb2d1b050b607b77cefba1f0178a
manuumqm <manuelamachado@aluno.senai.br>
2024-06-09 18:54:33 -0300

    feat: inputs de informações para adicionar um cartao

--- src/components/Payment/styles.js
+++ src/components/Payment/styles.js
@@ -2,7 +2,21 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#681111',
+        width: '100vw',
+        height: '100vh',
+    },
 
+    input: {
+        height: 40,
+        width: '80%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 });
 
