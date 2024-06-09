b59af64611387d903f9cccf6d6eed642a5a04d0a
manuumqm <manuelamachado@aluno.senai.br>
2024-06-09 16:21:58 -0300

    feat: css pagina de carrinho

---
+++ src/screens/Carrinho/index.jsx
@@ -0,0 +1,33 @@
+import { Text, View, Image, TouchableOpacity, ScrollView } from "react-native";
+import styles from "./styles";
+
+
+
+export default function Carrinho() {
+
+    return (
+        <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.title}>Seu Carrinho</Text>
+          <View style={styles.item}>
+          <Image style={styles.img}/> 
+            <View style={styles.texto}>
+            <Text>Item 1</Text>
+            <Text>R$10,00</Text>
+            </View>
+          </View>
+          <View style={styles.item}>
+        <Image style={styles.img} /> 
+          <View style={styles.texto}>
+            <Text>Item 2</Text>
+            <Text>R$15,00</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.checkoutButton}>
+            <Text style={styles.checkoutText}>Finalizar Compra</Text>
+          </TouchableOpacity>
+
+        </View>
+        </ScrollView>
+      )
+    };
---
+++ src/screens/Carrinho/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "#681111",
+        width: '100vw',
+        height: '100vh',
+    },
+    title: {
+        fontSize: "25px",
+    fontWeight: 'bold',
+    color: 'white'
+    },
+
+    item: {
+        width: '90vw',
+        height: '18vh',
+        marginTop: 100,
+        marginLeft: 8,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+
+    },
+    checkoutButton: {
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    checkoutText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+
+    texto: {
+        marginTop:-50,
+        marginLeft: 150,
+      },
+
+      
+  img: {
+    width: 90,
+    height: 90,
+    borderRadius: "20px",
+    marginTop: "15px",
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 10,
+  },
+
+    
+});
+
+export default styles;
