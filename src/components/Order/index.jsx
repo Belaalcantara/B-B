cc4c5298d3a5acf0449fe8aaad3d8e3bf0fbdc5a
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2024-06-09 22:09:59 -0300

    feat: botoes clear e continue

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
+
 function Orders({ data }) {
 
     const clear = async() => {
@@ -15,23 +16,12 @@ function Orders({ data }) {
     return (
         <ScrollView>
             <View style={styles.masterContainer}>
-                <Text style={styles.titulo}>FINALIZAR PEDIDO.</Text>
-                <View style={styles.container}>
-                    {products.map(product => (
-                        <View key={product.id} style={styles.subContainer}>
-                            <Image style={styles.image} source={{ uri: product.image }} />
-                            <Text>{product.name}</Text>
-                            <Text>{product.description}</Text>
-                            <Text>R${product.price}</Text>
-                            <TouchableOpacity style={styles.entryBtn}>
-                                <Text style={styles.entryText}>Adicionar ao Pedido</Text>
-                            </TouchableOpacity>
-                        </View>
-                    ))}
-                </View>
-
-                <TouchableOpacity onPress={registerUser} style={styles.entryBtn}>
-                    <Text style={styles.entryBtn}>Pedir!</Text>
+                
+             <TouchableOpacity onPress={clear} style={styles.button}>
+                    <Text style={styles.buttonText}>Limpar Pedido</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={continueBuy} style={styles.button}>
+                    <Text style={styles.buttonText}>Continuar Comprando</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
