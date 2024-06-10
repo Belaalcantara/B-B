a532ef20b89d6ef9fb300f6c0f803e36629d1e5e
Felipe Miotto <felipemiotto.564@gmail.com>
2024-06-09 22:55:59 -0300

    detalhes03

--- src/screens/Detalhes/index.jsx
+++ src/screens/Detalhes/index.jsx
@@ -1,9 +1,8 @@
-import { View, Text, Image, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import styles from "./styles";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../userContext";
-
 
 export default function DetailsOrders({ route }) {
     const { orderid } = route.params;
@@ -15,7 +14,7 @@ export default function DetailsOrders({ route }) {
     const fetchOrder = async () => {
         try {
             const orderDB = await axios.get(`http://localhost:4000/cart/user/${user.email}`);
-            if(orderDB.data) {
+            if (orderDB.data) {
                 let order = orderDB.data.orders.find((order) => order.order_id == orderid);
                 setOrder(order);
             }
@@ -27,30 +26,29 @@ export default function DetailsOrders({ route }) {
     useEffect(() => {
         fetchOrder();
     }, [orderid]);
-    console.log(order)
+
+    console.log(order);
+
     return (
         <View style={styles.container}>
-            {
-                order ? (
-                    <View>
+            {order ? (
+                <View style={styles.card}>
+                    <View style={styles.margem}>
                         <Text style={styles.name}>Restaurante: {order.restaurant_name}</Text>
-                        <Text style={styles.name}>ID do pedido: {order.order_idid}</Text>
-
-
-                        <View style={styles.containerDetalhes}>
-                            <Text style={styles.detalhes}>Detalhes do pedido: {order.products_details}</Text>
-                            <Text style={styles.detalhes}>Data do pedido: {order.order_date}</Text>
-                            <Text style={styles.detalhes}>Estado do pedido: {order.order_state}</Text>
-                        </View>
-                        <View style={styles.containerTotal}>
-                            <Text style={styles.detalhes}>Total do pedido: {order.total_price}</Text>
-                        </View>
                     </View>
-                ) : (
-                    <ActivityIndicator size="large" color="#dc341d" style={[styles.container, styles.horizontal]} />
-
-                )
-            }
+                    <View style={styles.containerDetalhes}>
+                        <Text style={styles.detalhes}>Detalhes do pedido: {order.products_details}</Text>
+                        <Text style={styles.detalhes}>Data do pedido: {order.order_date}</Text>
+                        <Text style={styles.detalhes}>Estado do pedido: {order.order_state}</Text>
+                    </View>
+                    <View style={styles.containerTotal}>
+                        <Text style={styles.id}>ID do pedido: {order.order_id}</Text>
+                        <Text style={styles.detalhes}>Total do pedido: {order.total_price}</Text>
+                    </View>
+                </View>
+            ) : (
+                <ActivityIndicator size="large" color="#dc341d" style={styles.horizontal} />
+            )}
         </View>
     );
 }
